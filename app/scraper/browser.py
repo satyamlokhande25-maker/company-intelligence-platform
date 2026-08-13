628308057b927f49de951fdf1eb861ad1f927c7b
Satyam Lokhande <satyamlokhande01@gmail.com>
2026-08-13 18:39:46 +0530

    Enable headless mode and server flags for selenium

--- app/scraper/browser.py
+++ app/scraper/browser.py
@@ -5,9 +5,13 @@ from selenium.webdriver.chrome.options import Options
 def create_driver():
     options = Options()
 
-    # Browser UI dikhega
-    # Headless mode baad mein enable kar sakte hain
-    options.add_argument("--start-maximized")
+    # Streamlit Cloud (Linux) ke liye Headless mode aur crucial flags zaroori hain
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--remote-debugging-port=9222")
 
     # Basic stability options
     options.add_argument("--disable-notifications")
